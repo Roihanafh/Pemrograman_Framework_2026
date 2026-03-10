02ea46e10f5cd2f45a580d523ac466429e82bfe8
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2026-03-11 05:35:41 +0700

    Jobsheet10

---
+++ Jobsheet10/praktikum/my-app/src/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+const profilPage = () => {
+    return (
+        <div>
+            <h1>Profil Mahasiswa</h1>
+            <p>Nama: Mohammad Roihan Al Fahmi Hidayatulloh</p>
+            <p>NIM: 2341720097</p>
+        </div>
+    );
+}
+
+export default profilPage;
