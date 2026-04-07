0cbf1cb4bbfafe07ca3489d7a4a230676a31b5e6
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2026-04-07 21:31:07 +0700

    Jobsheet 17

---
+++ Jobsheet17/praktikum/my-app/src/pages/editor/index.tsx
@@ -0,0 +1,16 @@
+const HalamanEditor = () => {
+  return (
+    <div>
+      <div className="editor">
+        <h1>Halaman Editor</h1>
+        <p>
+          Selamat datang di halaman editor! Halaman ini khusus untuk mengelola
+          dan mengedit konten aplikasi. Gunakan akses editor untuk meninjau,
+          memperbarui, dan memastikan kualitas konten yang ditampilkan.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HalamanEditor;
